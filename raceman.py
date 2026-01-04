5a8c01054595644dcbf181463c9b30e00d309de6
KarstenGorkow <karsten.gorkow@insite-gmbh.de>
2026-01-04 15:38:11 +0100

    Normal thread for start seq

--- raceman.py
+++ raceman.py
@@ -349,7 +349,7 @@ class RaceUI:
         self.handle_event("power_off")
         def seq():
             self.show_overlay(start_race_func)
-        t = threading.Thread(target=seq, daemon=True).start()
+        t = threading.Thread(target=seq).start()
         t.join()
         start_race_func()
 
